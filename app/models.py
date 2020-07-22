bee2bb856561750d75a251090b39533a24904acc
jtclayt <jt.clayton92@yahoo.com>
2020-07-22 16:35:19 -0700

    Create review manager for validations

--- app/models.py
+++ app/models.py
@@ -2,6 +2,29 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+class ReviewManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+
+        # Title validations
+        if len(postData['title']) < 3:
+            errors['title'] = 'Title must be 2 or more characters'
+
+        # Author validations
+        if 'author-input' not in postData or 'author-select' not in postData:
+            errors['author'] = 'Need to select or input an author.'
+        elif len(postData['author-input']) < 2:
+            errors['author'] = 'Author name must be 3 or more characters'
+
+        # Review validations
+        if len(postData['review']) < 10:
+            errors['review'] = 'Review must be 10 or more characters'
+        if postData['rating'] < 1 or postData['rating'] > 5:
+            errors['rating'] = 'Rating must be between 1 and 5'
+
+        return errors
+
+
 class Author(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -34,3 +57,5 @@ class Reviews(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = ReviewManager()
